7e49da073a106592b118cc49f81f6ee88e6aa8b7
Kevin Joseph Abalos <kevinabalos@gmail.com>
2018-05-11 05:29:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  var lowercase = "hello!"
-  var uppercase = "HELLO!"
-  if (lowercase.toLowerCase() === lowercase) {
-    return "I can't hear you!" }
-    else if (uppercase.toUpperCase() === uppercase) {
-      return "YES INDEED!"
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!" 
+  }
+    else if (string.toUpperCase() === string) {
+      return "YES INDEED!"  
     }
-    
+      else if (string === "I love you, Grandma.")
+        return "I love you, too."
 }
